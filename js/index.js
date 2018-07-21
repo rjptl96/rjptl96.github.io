a30bcf42e06459813f32def247f43346ad39e81c
Unknown <rjptl96@gmail.com>
2018-07-20 23:13:12 -0700

    More work on the carousel

--- js/index.js
+++ js/index.js
@@ -12,4 +12,15 @@
      $('.homePage').addClass('active');
  });
 
+ function bsCarouselAnimHeight() {
+     $('.carousel').carousel({
+         interval: 5000
+     }).on('slide.bs.carousel', function (e) {
+         var nextH = $(e.relatedTarget).height();
+         $(this).find('.active.item').parent().animate({
+             height: nextH
+         }, 500);
+     });
+ }
 
+ bsCarouselAnimHeight();
