c32fae049ab66e3300dd9f54f5a29411c2a864cf
Unknown <rjptl96@gmail.com>
2018-07-21 21:23:13 -0700

    Contacts Page updated

--- js/index.js
+++ js/index.js
@@ -11,3 +11,10 @@
      $('.navbar-nav li').removeClass('active');
      $('.homePage').addClass('active');
  });
+
+jQuery('.carousel').on('slide.bs.carousel', function (e) {
+   var nextH = jQuery(e.relatedTarget).height();
+   jQuery(this).find('.active.item').parent().animate({
+      height: nextH
+   }, 500);
+});
